8a1c97ebc32d3679e31b6386afda25b8070346d9
Egor Balyanov <egorilych09@gmail.com>
2026-04-05 19:37:04 +0500

    Fixed editing

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,13 @@ mod player;
 
 struct Settings {
     pub aspect: Vec2,
+    pub sc_width: f32,
+    pub sc_height: f32,
 }
 
 impl Settings {
     pub fn new() -> Self {
-        Settings { aspect: Vec2::ONE }
+        Settings { aspect: Vec2::ONE, sc_width: 640.0, sc_height: 480.0 }
     }
 }
 
@@ -44,16 +46,22 @@ impl EventHandler for Game {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         if ctx.mouse.button_pressed(MouseButton::Right) {
             let mouse_point = ctx.mouse.position();
-            let tile_x = mouse_point.x as usize / self.world.tile_size;
-            let tile_y = mouse_point.y as usize / self.world.tile_size;
-            let tile = self.world.get_mut(tile_x, tile_y).unwrap();
-            tile.id = world::BlockType::Stone;
+            let tile_x = mouse_point.x / self.world.tile_size as f32;
+            let tile_y = mouse_point.y / self.world.tile_size as f32;
+
+            if 0.0 <= mouse_point.x && mouse_point.x < self.settings.sc_width && 0.0 <= mouse_point.y && mouse_point.y < self.settings.sc_height {
+                let tile = self.world.get_mut(tile_x as usize, tile_y as usize).unwrap();
+                tile.id = world::BlockType::Stone;
+            }
         } else if ctx.mouse.button_pressed(MouseButton::Left) {
             let mouse_point = ctx.mouse.position();
-            let tile_x = mouse_point.x as usize / self.world.tile_size;
-            let tile_y = mouse_point.y as usize / self.world.tile_size;
-            let tile = self.world.get_mut(tile_x, tile_y).unwrap();
-            tile.id = world::BlockType::Air;
+            let tile_x = mouse_point.x / self.world.tile_size as f32;
+            let tile_y = mouse_point.y / self.world.tile_size as f32;
+
+            if 0.0 <= mouse_point.x && mouse_point.x < self.settings.sc_width && 0.0 <= mouse_point.y && mouse_point.y < self.settings.sc_height {
+                let tile = self.world.get_mut(tile_x as usize, tile_y as usize).unwrap();
+                tile.id = world::BlockType::Air;
+            }
         }
 
         self.world.update()?;
@@ -77,12 +85,21 @@ impl EventHandler for Game {
         canvas.finish(ctx)?;
         Ok(())
     }
+
+    fn resize_event(&mut self, _ctx: &mut Context, width: f32, height: f32) -> GameResult {
+        self.settings.sc_width = width;
+        self.settings.sc_height = height;
+        Ok(())
+    }
 }
 
 fn main() -> GameResult {
+    let sc_width: f32 = 1280.0;
+    let sc_height: f32 = 960.0;
+
     let (mut ctx, event_loop) = ContextBuilder::new("advent", "becheerful")
         .window_setup(ggez::conf::WindowSetup::default().title("Advent"))
-        .window_mode(ggez::conf::WindowMode::default().dimensions(640.0, 480.0))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(sc_width, sc_height))
         .build()
         .unwrap();
     ctx.fs.mount(&PathBuf::from("./resources"), true);
@@ -94,6 +111,8 @@ fn main() -> GameResult {
 
     let mut settings = Settings::new();
     settings.aspect = Vec2::splat(world.tile_size as f32 / atlas.tile_size as f32);
+    settings.sc_width = sc_width;
+    settings.sc_height = sc_height;
 
     let game = Game::new(&mut ctx, settings, atlas, world)?;
 
